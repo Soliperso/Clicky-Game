c478644f3a3842f2924de8f8cd31f7d2b227355c
Ed Chebli <chah762002@yahoo.fr>
2019-08-07 15:16:15 -0700

    Updates

---
+++ precache-manifest.d705caf571049901d15aded1bcd580b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef00ed1748369564a931831a772fe93",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "69144e12d97f8075dba9",
+    "url": "/clicky-game/static/css/main.ec2f6ec0.chunk.css"
+  },
+  {
+    "revision": "144b2d06e880dc0a7df8",
+    "url": "/clicky-game/static/js/2.1fd5fdae.chunk.js"
+  },
+  {
+    "revision": "69144e12d97f8075dba9",
+    "url": "/clicky-game/static/js/main.731aaa50.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
